92123509f5628d73197cd077f778d108e1e296c4
Jardar Iversen <jardar.iversen@gmail.com>
2022-09-19 08:54:23 +0200

    Reformating file

--- main.py
+++ main.py
@@ -21,20 +21,19 @@ screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
-
 game_is_on = True
 while game_is_on:
     screen.update()
     sleep(0.1)
     snake.move()
 
-    #Detects food collision
+    # Detects food collision
     if snake.head.distance(food) < 5:
         food.refresh()
         score.increment_score()
         snake.extend()
 
-    #Detects collision with wall
+    # Detects collision with wall
     if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() < -290 or snake.head.ycor() > 290:
         game_is_on = False
         score.game_over()
@@ -46,6 +45,5 @@ while game_is_on:
         elif snake.head.distance(segment) < 10:
             game_is_on = False
             score.game_over()
-
 
 screen.exitonclick()
